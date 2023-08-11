3255354ff7e497bc584af03b7d678faa952d435a
dpung1 <pung_12@naver.com>
2023-08-11 17:46:14 +0900

    feat : ToDoList Project

--- js/calendar.js
+++ js/calendar.js
@@ -54,44 +54,50 @@ function showCalendar() {
 }
 
 function handleDateClick(selectedDate) {
-    const todoListSideBar = document.querySelector(".todolist-sidebar"); 
+    const isToDoListSidebarOpen = "isToDoListSidebarOpen"; 
+
     const mainContainer = document.querySelector(".main-container");
-    const isToDoListSidebarOpen = "isToDoListSibebarOpen"; 
-    
+    const todoListSideBar = document.querySelector(".todolist-sidebar"); 
     const clickedDateDisplay = document.querySelector(".todolist-sidebar-date");
+    const todoInput = document.querySelector(".todolist-sidebar-items .text-input");
     
-    todoListSideBar.classList.toggle(isToDoListSidebarOpen);
+    todoInput.value = ""; // 사이드바 나올때마다 input창 초기화
     
-    if (todoListSideBar.classList.contains(isToDoListSidebarOpen)) {
-        mainContainer.style.transform = `translateX(-${150}px)`;
+    if (todoListSideBar.classList.contains(isToDoListSidebarOpen) && Calendar.getInstance().selectedDate - selectedDate === 0) {
+
+        todoListSideBar.classList.remove(isToDoListSidebarOpen)
+        mainContainer.style.transform = "none";
+
+    } else {
+
+        todoListSideBar.classList.add(isToDoListSidebarOpen)
+        
+        // 사이드바 나올 시 mainContainer안에 있는 달력도 옆으로 이동
+        mainContainer.style.transform = `translateX(-${150}px)`; 
         
         const year = calendarDate.getFullYear();
         const month = calendarDate.getMonth() + 1;
         
         clickedDateDisplay.textContent = `${year}년 ${month}월 ${selectedDate.getDate()}일`;
-        console.log(selectedDate)
         Calendar.getInstance().selectedDate = selectedDate;
-        console.log(Calendar.getInstance().selectedDate)
-
-    } else {
-        mainContainer.style.transform = "none";
+        
     }
+    
     TodoListService.getInstance().updateTodoList();
 }
-
+// 이전 달 이동
 function beforeMonth() {
     calendarDate = new Date(calendarDate.getFullYear(), calendarDate.getMonth() - 1, calendarDate.getDate());
     showCalendar();
 }
 
 document.getElementById("beforebtn").addEventListener("click", beforeMonth);
-
+// 다음 달 이동
 function nextMonth() {
     calendarDate = new Date(calendarDate.getFullYear(), calendarDate.getMonth() + 1, calendarDate.getDate());
     showCalendar();
 }
 
 document.getElementById("nextbtn").addEventListener("click", nextMonth);
-
 
 showCalendar();
