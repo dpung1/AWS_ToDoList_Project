7b467c259df701cd37b070d734b4ea1cce11b584
dpung1 <pung_12@naver.com>
2023-08-26 12:17:28 +0900

    feat : selectedDay Point 기능 추가

--- js/calendar.js
+++ js/calendar.js
@@ -55,59 +55,69 @@ function showCalendar() {
         contentText.textContent = currentDate.getDate();
         contentText.addEventListener("click", () => {
             const selectedDate = new Date(`${calendarDate.getFullYear()}/${calendarDate.getMonth() + 1}/${contentText.textContent}`);
-            handleDateClick(selectedDate);
+            handleDateClick(selectedDate, contentText);
         });
+        
+        ;
         // 오늘 날짜 표시
         if(
             currentDate.getDate() === today.getDate() &&
             currentDate.getMonth() === today.getMonth() &&
             currentDate.getFullYear() === today.getFullYear()
-        ) {
-
-            contentText.classList.add("today")
+            ) {
+                
+                contentText.classList.add("today")
+            }
+            
+            content.appendChild(contentText);
+            cell.appendChild(content);
+            weekRow.appendChild(cell);
+            // 한 주의 끝이거나 마지막날 줄 변경
+            if (currentDate.getDay() === 6 || currentDate.getTime() === lastDay.getTime()) {
+                calendarBody.appendChild(weekRow);
+                weekRow = document.createElement("tr");
+            }
+            
+            currentDate.setDate(currentDate.getDate() + 1);
         }
-
-        content.appendChild(contentText);
-        cell.appendChild(content);
-        weekRow.appendChild(cell);
-        // 한 주의 끝이거나 마지막날 줄 변경
-        if (currentDate.getDay() === 6 || currentDate.getTime() === lastDay.getTime()) {
-            calendarBody.appendChild(weekRow);
-            weekRow = document.createElement("tr");
-        }
-
-        currentDate.setDate(currentDate.getDate() + 1);
+        
+        monthDisplay.textContent = `${calendarDate.getFullYear()}년 ${calendarDate.getMonth() + 1}월`;
     }
     
-    monthDisplay.textContent = `${calendarDate.getFullYear()}년 ${calendarDate.getMonth() + 1}월`;
-}
-
-// 날짜 클릭 이벤트
-function handleDateClick(selectedDate) {
-    const isToDoListSidebarOpen = "isToDoListSidebarOpen";
-    const isAllToDoListSidebarOpen = "isAllToDoListSidebarOpen";
-
-    const todoListSideBar = document.querySelector(".todolist-sidebar"); 
-    const clickedDateDisplay = document.querySelector(".todolist-sidebar-date");
-    const todoInput = document.querySelector(".todolist-sidebar-items .text-input");
-    const allTodoListSideBar = document.querySelector(".all-todolist-sidebar");
-    // 사이드바 나올때마다 input창 초기화
-    todoInput.value = ""; 
-    // 할 일 목록 사이드바 열고 닫기
-    if (todoListSideBar.classList.contains(isToDoListSidebarOpen) && Calendar.getInstance().selectedDate - selectedDate === 0) {
-        todoListSideBar.classList.remove(isToDoListSidebarOpen);
-        allTodoListSideBar.classList.remove(isAllToDoListSidebarOpen);
+    // 날짜 클릭 이벤트
+    function handleDateClick(selectedDate, contentText) {
+        const isToDoListSidebarOpen = "isToDoListSidebarOpen";
+        const isAllToDoListSidebarOpen = "isAllToDoListSidebarOpen";
+        
+        const todoListSideBar = document.querySelector(".todolist-sidebar"); 
+        const clickedDateDisplay = document.querySelector(".todolist-sidebar-date");
+        const todoInput = document.querySelector(".todolist-sidebar-items .text-input");
+        const allTodoListSideBar = document.querySelector(".all-todolist-sidebar");
+        
+        // 사이드바 나올때마다 input창 초기화
+        todoInput.value = "";
+        
+        // 선택한 날짜 표시하기
+        const selectedDayPoint = document.querySelectorAll(".select")
+            selectedDayPoint.forEach(point => point.classList.remove("select"));
+            contentText.classList.add("select")
+        
+        // 할 일 목록 사이드바 열고 닫기
+        if (todoListSideBar.classList.contains(isToDoListSidebarOpen) && Calendar.getInstance().selectedDate - selectedDate === 0) {
+            todoListSideBar.classList.remove(isToDoListSidebarOpen);
+            allTodoListSideBar.classList.remove(isAllToDoListSidebarOpen);
+            contentText.classList.remove("select")
 
     } else {
         todoListSideBar.classList.add(isToDoListSidebarOpen);
-        
+
         const year = calendarDate.getFullYear();
         const month = calendarDate.getMonth() + 1;
         
         clickedDateDisplay.textContent = `${year}년 ${month}월 ${selectedDate.getDate()}일`;
         Calendar.getInstance().selectedDate = selectedDate;
     }
-    
+
     TodoListService.getInstance().updateTodoList();
 }
 
